ddb72f8a493c00029a43591d28a4903b266a3170
yanniks <kontakt@yanniks.de>
2014-03-31 17:29:05 +0200

    initial CyanDream commit

--- cd.mk
+++ cd.mk
@@ -1,18 +1,18 @@
 $(call inherit-product, device/htc/m4/full_m4.mk)
 
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/cyandream/config/gsm.mk)
 
 PRODUCT_RELEASE_NAME := m4
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cyandream/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/htc/m4/device.mk)
 
 # Device naming
 PRODUCT_DEVICE := m4
-PRODUCT_NAME := cm_m4
+PRODUCT_NAME := cd_m4
 PRODUCT_BRAND := htc
 PRODUCT_MODEL := One Mini
 PRODUCT_MANUFACTURER := HTC
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo cm_m4-eng
+add_lunch_combo cd_m4-eng
